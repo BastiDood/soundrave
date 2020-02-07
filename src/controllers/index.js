fad9880d8cb42f5b2be0aa97755f6325d6dc38ad
Basti Ortiz <ortizbasti16@gmail.com>
2020-02-08 01:32:29 +0800

    Refactor: rename page title

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Spotify Timeline' });
 });
 
 export { router };
