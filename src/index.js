c724721cb74d8854e591044ca05e8c1868677008
Basti Ortiz <ortizbasti16@gmail.com>
2020-02-08 15:23:41 +0800

    Style: chain together similar Express configurations

--- src/index.js
+++ src/index.js
@@ -14,24 +14,26 @@ const { PORT } = process.env;
 const app = express();
 
 // Set render engine
-app.set('view engine', 'ejs');
-app.set('views', 'src/views');
+app
+  .set('view engine', 'ejs')
+  .set('views', 'src/views');
 
 // Activate ecurity headers
-app.disable('x-powered-by');
-app.use(helmet());
-app.use(helmet.permittedCrossDomainPolicies());
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    formAction: ['\'self\''],
-    defaultSrc: ['\'self\''],
-    frameAncestors: ['\'none\'']
-  }
-}));
-app.use(helmet.noCache());
-app.use(helmet.referrerPolicy({
-  policy: 'no-referrer'
-}));
+app
+  .disable('x-powered-by')
+  .use(helmet())
+  .use(helmet.permittedCrossDomainPolicies())
+  .use(helmet.contentSecurityPolicy({
+    directives: {
+      formAction: ['\'self\''],
+      defaultSrc: ['\'self\''],
+      frameAncestors: ['\'none\'']
+    }
+  }))
+  .use(helmet.noCache())
+  .use(helmet.referrerPolicy({
+    policy: 'no-referrer'
+  }));
 
 // Set public files directory
 app.use(express.static('public', { index: false }));
@@ -41,6 +43,4 @@ app.use(express.static('public', { index: false }));
 app.use('/', router);
 
 // Listen to the servers
-app.listen(PORT, () => {
-  console.log(`Server started at port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
