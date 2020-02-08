49b7c943a6e1eb3153e111856955f5a8d1893eee
Basti Ortiz <ortizbasti16@gmail.com>
2020-02-08 15:11:54 +0800

    Feature: redirect user to Spotify authorization page

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,3 @@
 // TODO: Redirect user to Spotify Authorization URL
 const el_butt = document.getElementsByTagName('button')[0];
-el_butt.addEventListener('click', () => alert('Test'));
+el_butt.addEventListener('click', () => window.location.href = '/login');
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,34 @@
+// NATIVE IMPORTS
+import querystring from 'querystring';
+
+// DEPENDENCIES
+import dotenv from 'dotenv';
 import express from 'express';
 
-const router = express.Router();
+// Initialize .env
+dotenv.config();
 
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Spotify Timeline' });
-});
+// GLOBAL VARIABLES
+const router = express.Router();
+const { CLIENT_ID } = process.env;
+
+router
+  .get('/', (req, res) => {
+    res.render('index', { title: 'Spotify Timeline' });
+  })
+  .get('/login', (req, res) => {
+    const AUTHORIZATION_CODE = req.query['code'];
+    if (AUTHORIZATION_CODE)
+      res.json(AUTHORIZATION_CODE);
+    else {
+      const params = {
+        client_id: CLIENT_ID,
+        response_type: 'code',
+        redirect_uri: 'http://localhost:3000/login',
+        scope: 'user-follow-read'
+      };
+      res.redirect(`https://accounts.spotify.com/authorize?${querystring.stringify(params)}`);
+    }
+  });
 
 export { router };
