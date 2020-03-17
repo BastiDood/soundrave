ad8d3ec615e07b0e368a995c381785ba9360867c
Basti Ortiz <ortizbasti16@gmail.com>
2020-03-17 23:29:23 +0800

    Feature: add field for Spotify URLs

--- src/models/Core.js
+++ src/models/Core.js
@@ -9,6 +9,7 @@ const ImageSchema = new mongoose.Schema({
 const ArtistSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: { type: String, required: true },
+  url: { type: String, required: true },
   followers: { type: Number, required: true },
   popularity: { type: Number, required: true, min: 0, max: 100 },
   images: [ ImageSchema ]
@@ -17,6 +18,7 @@ const ArtistSchema = new mongoose.Schema({
 const ReleaseSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   title: { type: String, required: true },
+  url: { type: String, required: true },
   releaseDate: { type: Date, required: true },
   datePrecision: { type: String, enum: [ 'year', 'month', 'day' ], required: true },
   images: [ { type: ImageSchema, required: true } ],
