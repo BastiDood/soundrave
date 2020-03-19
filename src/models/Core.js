bde1f2c9413088e2cbee0cbb5fe11783ead53024
Basti Ortiz <ortizbasti16@gmail.com>
2020-03-19 22:57:03 +0800

    Feature: add fields for `album_type` and `availableCountries`

--- src/models/Core.js
+++ src/models/Core.js
@@ -13,8 +13,10 @@
  * @typedef {Object} ReleaseObject
  * @property {string} _id - Spotify ID of the release
  * @property {string} title
+ * @property {'album'|'single'|'compilation'} albumType
  * @property {number} releaseDate - Represented as milliseconds since Unix time (in milliseconds)
  * @property {'year'|'month'|'day'} datePrecision - Determines precision of date
+ * @property {string[]} availableCountries
  * @property {SpotifyApi.ImageObject[]} images
  * @property {ArtistID[]} artists - Spotify IDs of artists
  */
@@ -38,10 +40,12 @@ const ArtistSchema = new mongoose.Schema({
 const ReleaseSchema = new mongoose.Schema({
   _id: { type: String, required: true },
   title: { type: String, required: true },
+  albumType: { type: String, enum: [ 'album', 'single', 'compilation' ], required: true },
   releaseDate: { type: String, required: true },
   datePrecision: { type: String, enum: [ 'year', 'month', 'day' ], required: true },
+  availableCountries: { type: [ String ], required: true },
   images: [ { type: ImageSchema, required: true } ],
-  artists: { type: [ { type: mongoose.Schema.Types.ObjectId, ref: 'Artist', required: true } ], required: true }
+  artists: { type: [ { type: String, ref: 'Artist', required: true } ], required: true }
 });
 
 export const Artist = mongoose.model('Artist', ArtistSchema);
