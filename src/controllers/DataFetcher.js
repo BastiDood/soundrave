5b7191007fc69027296cc33a49cc8bd755575ad1
Basti Ortiz <ortizbasti16@gmail.com>
2020-03-19 22:58:14 +0800

    Feature: retrieve releases by cache or fetch from Spotify otherwise
    * Session now includes list of followed artists (by ID)
    * All fetches cache by default once successful

--- src/controllers/DataFetcher.js
+++ src/controllers/DataFetcher.js
@@ -12,6 +12,8 @@
  * A new refresh token might be returned too.)
  */
 
+// TODO: Handle rate-limited fetches
+
 // DEPENDENCIES
 import fetch from 'node-fetch';
 
@@ -20,33 +22,38 @@ import * as CoreModels from '../models/Core.js';
 
 export class DataFetcher {
   /** @param {AccessToken} token - Access token for the Spotify API */
-  constructor(token) { this._token = token; }
-
-  /** @param {CoreModels.ArtistObject[]} artists */
-  async _cacheFollowedArtists(artists) {
-    const promises = artists.map(artist => {
-      const doc = new CoreModels.Artist(artist);
-      return doc.save();
-    });
-    await Promise.all(promises);
+  constructor(token) {
+    this._FETCH_OPTIONS = {
+      method: 'GET',
+      headers: { Authorization: `${token.token_type} ${token.access_token}` }
+    };
   }
 
-  /**
-   * @param {number} limit - Maximum number of artists to be fetched.
-   * @returns {Promise<CoreModels.ArtistObject[]>}
-   */
-  async _fetchFollowedArtists(limit) {
-    // TODO: Cache the session user's followed artists
-    const FETCH_OPTIONS = {
-      method: 'GET',
-      headers: { Authorization: `${this._token.token_type} ${this._token.access_token}` }
-    };
+  /** @param {CoreModels.ArtistObject[]} artists */
+  _cacheArtistObjects(artists) {
+    const promises = artists.map(artist =>
+      CoreModels.Artist.findByIdAndUpdate(artist._id, artist, { upsert: true }).exec());
+    return Promise.allSettled(promises);
+  }
+
+  /** @param {CoreModels.ReleaseObject[]} releases */
+  _cacheReleaseObjects(releases) {
+    const promises = releases.map(release => CoreModels.Release
+      .findByIdAndUpdate(release._id, release, { upsert: true })
+      .exec());
+    return Promise.allSettled(promises);
+  }
+
+  /** @returns {Promise<CoreModels.ArtistObject[]>} */
+  async _fetchFollowedArtists() {
     /** @type {CoreModels.ArtistObject[]} */
     let followedArtists = [];
-    let next = `https://api.spotify.com/v1/me/following?type=artist&limit=${limit}`;
+    let next = 'https://api.spotify.com/v1/me/following?type=artist&limit=50';
+
+    // Retrieve all followed artists
     while (next) {
       /** @type {SpotifyApi.UsersFollowedArtistsResponse} */
-      const { artists } = await fetch(next, FETCH_OPTIONS).then(res => res.json());
+      const { artists } = await fetch(next, this._FETCH_OPTIONS).then(res => res.json());
       const transformedArtistData = artists.items
         .map(artist => ({
           _id: artist.id,
@@ -58,7 +65,90 @@ export class DataFetcher {
       followedArtists = followedArtists.concat(transformedArtistData);
       next = artists.next;
     }
-    this._cacheFollowedArtists(followedArtists);
+
+    this._cacheArtistObjects(followedArtists);
     return followedArtists;
+  }
+
+  /**
+   * Invoke this method **sparingly**. It greatly contributes to the rate limit.
+   * @param {string} id - Spotify Artist ID
+   * @returns {Promise<CoreModels.ReleaseObject[]>}
+   */
+  async _fetchReleasesByArtistID(id) {
+    // TODO: Get IP location and filter `market`. See https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Current_codes.
+    /** @type {CoreModels.ReleaseObject[]} */
+    let releases = [];
+    let next = `https://api.spotify.com/v1/artists/${id}/albums?include_groups=album,single&limit=50`;
+
+    // Retrieve all releases by the artist
+    while (next) {
+      /** @type {SpotifyApi.ArtistsAlbumsResponse} */
+      const json = await fetch(next, this._FETCH_OPTIONS).then(res => res.json());
+
+      /** @type {CoreModels.ReleaseObject[]} */
+      const transformedReleaseData = json.items
+        .map(release => ({
+          _id: release.id,
+          title: release.name,
+          albumType: release.album_type,
+          releaseDate: Number(Date.parse(release.release_date)),
+          datePrecision: /** @type {'year'|'month'|'day'} */ (release.release_date_precision),
+          availableCountries: release.available_markets,
+          images: release.images,
+          artists: release.artists.map(artist => artist.id)
+        }));
+      releases = releases.concat(transformedReleaseData);
+      next = json.next;
+    }
+
+    this._cacheReleaseObjects(releases);
+    return releases;
+  }
+
+  /**
+   * Retrieve releases of artists by cache or Spotify API
+   * @param {string} ids - Spotify Artist IDs
+   * @returns {Promise<CoreModels.ReleaseObject[]>}
+   */
+  async getReleasesByArtistIDs(ids) {
+    // Determine which releases (by artist IDs) are in the cache
+    // See https://2ality.com/2015/01/es6-set-operations.html#difference.
+    /** @type {CoreModels.ReleaseObject[]} */
+    // @ts-ignore
+    const cachedReleases = await CoreModels.Release
+      .find({ artists: { $in: ids } })
+      .populate('artists')
+      .exec();
+    const allSet = new Set(ids);
+    const cachedSet = new Set(cachedReleases.map(release => release._id));
+    const differenceSet = new Set([...allSet].filter(id => !cachedSet.has(id)));
+
+    // Fetch Spotify API for those that have not been cached yet
+    const remainingArtistIDs = Array.from(differenceSet);
+    const promises = remainingArtistIDs.map(id => this._fetchReleasesByArtistID(id));
+    const settledPromises = await Promise.allSettled(promises);
+
+    // Separate successful requests from failed requests
+    /** @type {CoreModels.ReleaseObject[][]} */
+    const successfulReqs = [];
+    const failedReqs = [];
+    for (const promise of settledPromises)
+      if (promise.status === 'fulfilled')
+        successfulReqs.push(promise.value);
+      else if (promise.status === 'rejected')
+        failedReqs.push(promise.reason);
+
+    // TODO: Handle errors, especially in cases of errors
+    console.table(failedReqs);
+
+    // Merge successful requests with the cached releases
+    // by removing duplicate releases (by ID)
+    // See https://reactgo.com/removeduplicateobjects/#here-is-my-implementation-to-remove-the-duplicate-objects-from-an-array.
+    /** @type {{ [string: string]: CoreModels.ReleaseObject }} */
+    const lookupObject = {};
+    for (const release of [ ...cachedReleases, ...successfulReqs.flat() ])
+      lookupObject[release._id] = release;
+    return Object.values(lookupObject);
   }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 // NATIVE IMPORTS
 import querystring from 'querystring';
+import { URLSearchParams } from 'url';
 
 // DEPENDENCIES
 import dotenv from 'dotenv';
@@ -23,17 +24,40 @@ const REQUEST_AUTHORIZATION_ENDPOINT = `https://accounts.spotify.com/authorize?$
   redirect_uri: REDIRECT_URI,
   scope: 'user-follow-read'
 })}`;
+const ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
 
 router
   .get('/', async (req, res) => {
-    if (!req.session.isLoggedIn) {
+    // Shorthand for session object
+    const { session } = req;
+    // Reject all users that have not been logged in
+    if (!session.isLoggedIn) {
       res.render('index');
       return;
     }
 
-    const dataFetcher = new DataFetcher(req.session.token);
-    const artists = await dataFetcher._fetchFollowedArtists(50);
-    res.render('index', { artists });
+    // Initialize data fetcher
+    const dataFetcher = new DataFetcher(session.token);
+
+    // Check if there are no cached artists in the session
+    const TODAY = new Date().getTime();
+    if (!(session.followedArtists && session.followedArtists.ids)
+        || session.followedArtists.retrievalDate + ONE_WEEK > TODAY
+    ) {
+      // Fetch followed artists
+      const artists = await dataFetcher._fetchFollowedArtists();
+      const artistIDs = artists.map(artist => artist._id);
+
+      // Cache artist IDs to current session to save on memory
+      session.followedArtists = {
+        ids: artistIDs,
+        retrievalDate: TODAY
+      };
+    }
+
+    // Get releases
+    const releases = await dataFetcher.getReleasesByArtistIDs(session.followedArtists.ids);
+    res.render('index', { releases });
   })
   .get('/login', (req, res) => {
     if (req.session.isLoggedIn)
@@ -46,28 +70,28 @@ router
     // Check if authorization code exists
     const AUTHORIZATION_CODE = req.query['code'];
     if (AUTHORIZATION_CODE) {
-      const urlEncodedParams = new URLSearchParams(
-        querystring.stringify({
-          grant_type: 'authorization_code',
-          code: AUTHORIZATION_CODE,
-          redirect_uri: REDIRECT_URI,
-          client_id: CLIENT_ID,
-          client_secret: CLIENT_SECRET
-        })
-      );
       const response = await fetch(REQUEST_TOKEN_ENDPOINT, {
         method: 'POST',
-        body: urlEncodedParams,
+        body: new URLSearchParams(
+          querystring.stringify({
+            grant_type: 'authorization_code',
+            code: AUTHORIZATION_CODE,
+            redirect_uri: REDIRECT_URI,
+            client_id: CLIENT_ID,
+            client_secret: CLIENT_SECRET
+          })
+        ),
       });
 
       // TODO: Use refresh tokens. Do not log user out after expiry.
+      // TODO: Take note of duplicate markets
       // Set session data
       /** @type {import('../controllers/DataFetcher').AccessToken} */
       const token = await response.json();
       req.session.token = token;
       req.session.cookie.maxAge = token.expires_in * 1e3;
       req.session.isLoggedIn = true;
-      req.session.save(console.error);
+      req.session.save();
     }
 
     res.redirect('/');
