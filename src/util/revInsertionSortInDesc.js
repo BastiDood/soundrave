eb98041cd3a8b4745ebc223282ee7d9df54719ac
Basti Ortiz <ortizbasti16@gmail.com>
2020-04-04 17:35:44 +0800

    Refactor: store transformed temporary to a variable

--- src/util/revInsertionSortInDesc.js
+++ src/util/revInsertionSortInDesc.js
@@ -5,16 +5,17 @@
  * dates, this means from "most recent" to "oldest".
  * @template T
  * @param {T[]} arr
- * @param {(obj: T) => any} transformFunc 
+ * @param {(obj: T) => number} transformFunc 
  */
 export function revInsertionSortInDesc(arr, transformFunc = x => x) {
   const { length } = arr;
 
   for (let i = length - 2; i >= 0; --i) {
     const temp = arr[i];
+    const transformedTemp = transformFunc(temp);
     let j = i + 1;
 
-    while (j < length && transformFunc(temp) < transformFunc(arr[j])) {
+    while (j < length && transformedTemp < transformFunc(arr[j])) {
       arr[j - 1] = arr[j];
       ++j;
     }
