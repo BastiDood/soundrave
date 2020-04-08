8050dcf96533d9209f0a3b29d85ad42a8e7aef16
Basti Ortiz <ortizbasti16@gmail.com>
2020-04-09 00:23:44 +0800

    Docs: add to the global scope

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -87,3 +87,10 @@ declare interface PopulatedReleaseObject extends ReleaseObject {
   /** Spotify object representation of artists */
   artists: ArtistObject[];
 }
+
+declare interface FollowedArtistsCache {
+  /** List of cached Spotify artist IDs. */
+  ids: string[];
+  /** Last retrieval date (in milliseconds since UNIX epoch). */
+  retrievalDate: number;
+}
