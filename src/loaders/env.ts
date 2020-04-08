d43d070ecc0fc5b51b610f24c4ce339cca79d352
Basti Ortiz <ortizbasti16@gmail.com>
2020-04-08 16:25:41 +0800

    Feature: centralize the access to environment variables

---
+++ src/loaders/env.ts
@@ -0,0 +1,43 @@
+// NATIVE IMPORTS
+import assert from 'assert';
+
+// DEPENDENCIES
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {
+  NODE_ENV,
+  PORT,
+  DEFAULT_COUNTRY,
+  MONGO_DB_CACHE_URL,
+  MONGO_DB_SESSION_URL,
+  MONGO_DB_SESSION_SECRET,
+  COOKIE_SECRET,
+  CLIENT_ID,
+  CLIENT_SECRET,
+} = process.env;
+
+assert(NODE_ENV);
+assert(PORT);
+assert(DEFAULT_COUNTRY);
+assert(MONGO_DB_CACHE_URL);
+assert(MONGO_DB_SESSION_URL);
+assert(MONGO_DB_SESSION_SECRET);
+assert(COOKIE_SECRET);
+assert(CLIENT_ID);
+assert(CLIENT_SECRET);
+
+const env = {
+  NODE_ENV,
+  PORT,
+  DEFAULT_COUNTRY,
+  MONGO_DB_CACHE_URL,
+  MONGO_DB_SESSION_URL,
+  MONGO_DB_SESSION_SECRET,
+  COOKIE_SECRET,
+  CLIENT_ID,
+  CLIENT_SECRET,
+};
+
+export { env };
