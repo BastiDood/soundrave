cb486679d90a8c95a8ac11bbf85ffa1291c683e3
Basti Ortiz <ortizbasti16@gmail.com>
2020-04-09 00:21:06 +0800

    Docs: expose data structure for followed artists cache

--- typings/global-plugin.d.ts
+++ typings/global-plugin.d.ts
@@ -1,15 +1,18 @@
 import 'express';
+
+declare interface FollowedArtistsCache {
+  /** List of cached Spotify artist IDs. */
+  ids: string[];
+  /** Last retrieval date (in milliseconds since UNIX epoch). */
+  retrievalDate: number;
+}
+
 declare global {
   namespace Express {
     interface Session {
       token?: { spotify: SpotifyAccessToken };
       isLoggedIn?: boolean;
-      followedArtists?: {
-        /** List of cached Spotify artist IDs. */
-        ids: string[];
-        /** Last retrieval date (in milliseconds since UNIX epoch). */
-        retrievalDate: number;
-      };
+      followedArtists?: FollowedArtistsCache;
     }
   }
 }
