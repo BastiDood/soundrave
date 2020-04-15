9995ffb3344ca782beecd127a2d38805e4ad65ba
Basti Ortiz <ortizbasti16@gmail.com>
2020-04-15 22:33:27 +0800

    Refactor: deny entry to routes if the client is in the wrong state

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -39,72 +39,75 @@ router
   })
   .get('/login', (req, res) => {
     if (req.session?.isLoggedIn)
-      res.redirect('/');
+      res.sendStatus(404);
     else
       res.redirect(SpotifyAPI.AUTH_ENDPOINT);
   })
   .get('/callback', async (req: express.Request<{}, {}, {}, AuthorizationResult>, res) => {
-    // TODO: Check if request is from Spotify accounts using `state` parameter
-    // Check if authorization code exists
     const { session } = req;
     const authorization = req.query;
-    // TODO: Return 404 instead of redirecting to home
-    if (session && 'code' in authorization) {
-      // Exchange the authorization code for an access token
-      const tokenResult = await SpotifyAPI.exchangeCodeForAccessToken(authorization.code);
 
-      // TODO: Handle any authorization errors
-      assert(tokenResult.ok);
-
-      // Generate new session when the user logs in
-      await promisify(session.regenerate.bind(session))();
-
-      // Initialize session data
-      const { value: token } = tokenResult;
-      const ONE_HOUR = token.expires_in * 1e3;
-      session.token = Object.create(null);
-      assert(session.token);
-      session.token.spotify = {
-        accessToken: token.access_token,
-        refreshToken: token.refresh_token,
-        scope: token.scope,
-        expiresAt: Date.now() + ONE_HOUR,
-      };
-      session.cookie.maxAge = ONE_HOUR;
-      session.isLoggedIn = true;
-
-      // Initialize session cache
-      session.cache = Object.create(null);
-      assert(session.cache);
-      session.cache.followedArtists = {
-        ids: [],
-        retrievalDate: -Infinity,
-      };
-      assert(session.cache.followedArtists);
-      session.cache.user = Object.create(null);
-      assert(session.cache.user);
-
-      // Retrieve the real country code
-      const api = new SpotifyAPI(session.token.spotify);
-      const userResult = await api.fetchUserProfile();
-      // TODO: Handle any errors during the fetch
-      assert(userResult.ok);
-      session.cache.user.country = userResult.value.country;
-
-      // Retrieve followed artists
-      const retriever = new DataRetriever(new SpotifyAPI(session.token.spotify), session.cache as Required<SessionCache>);
-      const followedArtists = await retriever.getFollowedArtistIDs();
-      session.cache.followedArtists = {
-        ids: followedArtists.ids,
-        retrievalDate: followedArtists.retrievalDate,
-      };
-
-      // TODO: Handle any error from `followedArtists.error`
-      assert(!followedArtists.error);
-
-      // Explicitly save session data due to redirect
-      await promisify(session.save.bind(session))();
+    // TODO: Check if request is from Spotify accounts using `state` parameter
+    // Check if authorization code exists
+    if (!session || !('code' in authorization)) {
+      res.sendStatus(404);
+      return;
     }
+
+    // Exchange the authorization code for an access token
+    const tokenResult = await SpotifyAPI.exchangeCodeForAccessToken(authorization.code);
+
+    // TODO: Handle any authorization errors
+    assert(tokenResult.ok);
+
+    // Generate new session when the user logs in
+    await promisify(session.regenerate.bind(session))();
+
+    // Initialize session data
+    const { value: token } = tokenResult;
+    const ONE_HOUR = token.expires_in * 1e3;
+    session.token = Object.create(null);
+    assert(session.token);
+    session.token.spotify = {
+      accessToken: token.access_token,
+      refreshToken: token.refresh_token,
+      scope: token.scope,
+      expiresAt: Date.now() + ONE_HOUR,
+    };
+    session.cookie.maxAge = ONE_HOUR;
+    session.isLoggedIn = true;
+
+    // Initialize session cache
+    session.cache = Object.create(null);
+    assert(session.cache);
+    session.cache.followedArtists = {
+      ids: [],
+      retrievalDate: -Infinity,
+    };
+    assert(session.cache.followedArtists);
+    session.cache.user = Object.create(null);
+    assert(session.cache.user);
+
+    // Retrieve the real country code
+    const api = new SpotifyAPI(session.token.spotify);
+    const userResult = await api.fetchUserProfile();
+    // TODO: Handle any errors during the fetch
+    assert(userResult.ok);
+    session.cache.user.country = userResult.value.country;
+
+    // Retrieve followed artists
+    const retriever = new DataRetriever(new SpotifyAPI(session.token.spotify), session.cache as Required<SessionCache>);
+    const followedArtists = await retriever.getFollowedArtistIDs();
+    session.cache.followedArtists = {
+      ids: followedArtists.ids,
+      retrievalDate: followedArtists.retrievalDate,
+    };
+
+    // TODO: Handle any error from `followedArtists.error`
+    assert(!followedArtists.error);
+
+    // Explicitly save session data due to redirect
+    await promisify(session.save.bind(session))();
 
     // TODO: Handle error if `error in authorization`
 
