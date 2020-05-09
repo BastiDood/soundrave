348284d8c06b2051f922e8c18089420e5a59ee8c
Basti Ortiz <ortizbasti16@gmail.com>
2020-05-09 23:16:06 +0800

    Feature: improve load performance by marking scripts as `async`

--- public/js/main.ts
+++ public/js/main.ts
@@ -1,3 +1,5 @@
-const el_butt = document.getElementsByTagName('button')[0];
-if (el_butt)
-  el_butt.addEventListener('click', () => { window.location.href = '/login'; });
+document.addEventListener('DOMContentLoaded', () => {
+  const el_butt = document.getElementsByTagName('button')[0];
+  if (el_butt)
+    el_butt.addEventListener('click', () => { window.location.href = '/login'; });
+}, { once: true });
