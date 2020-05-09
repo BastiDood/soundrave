909866a35777ae34f333bacbd6d91ddd96b32218
Basti Ortiz <ortizbasti16@gmail.com>
2020-05-10 00:31:40 +0800

    Feature: use passive event listeners

--- public/js/main.ts
+++ public/js/main.ts
@@ -2,4 +2,4 @@ document.addEventListener('DOMContentLoaded', () => {
   const el_butt = document.getElementsByTagName('button')[0];
   if (el_butt)
     el_butt.addEventListener('click', () => { window.location.href = '/login'; });
-}, { once: true });
+}, { once: true, passive: true });
