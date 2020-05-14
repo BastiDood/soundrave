15950904b5765dd0904eff5ff2b4d913736965de
Basti Ortiz <ortizbasti16@gmail.com>
2020-05-14 17:06:01 +0800

    Chore: remove utility functions that are no longer needed

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,11 +1,7 @@
 import { formatEndpoint } from './formatEndpoint';
-import { removeDuplicatesFromArrays } from './removeDuplicatesFromArrays';
-import { revInsertionSortInDesc } from './revInsertionSortInDesc';
 import { subdivideArray } from './subdivideArray';
 
 export {
   formatEndpoint,
-  removeDuplicatesFromArrays,
-  revInsertionSortInDesc,
   subdivideArray,
 };
