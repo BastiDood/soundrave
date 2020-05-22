ee2edc39b120a11024414d5e9b8612baaa68d3ce
Basti Ortiz <ortizbasti16@gmail.com>
2020-05-22 17:31:10 +0800

    WIP: add expiration for sessions

--- src/db/schemas/session/SessionSchema.ts
+++ src/db/schemas/session/SessionSchema.ts
@@ -1,12 +1,16 @@
 import { Schema } from 'mongoose';
 import { AccessTokenSchema } from './AccessTokenSchema';
 
+// Long-lived Sessions
 export const BaseSessionSchema = new Schema({
   _id: { type: String, required: true },
   userID: { type: String, required: true },
   token: { type: Map, of: AccessTokenSchema, required: true },
+  createdAt: { type: Date, default: Date.now, expires: '14d' },
 });
 
+// Short-lived Sessions
 export const LoginSessionSchema = new Schema({
   loginNonce: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now, expires: '5m' },
 });
