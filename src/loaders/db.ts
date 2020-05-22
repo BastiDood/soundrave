9e23b01b9c8e1c362f566ccf8dab2283ddce9b5a
Basti Ortiz <ortizbasti16@gmail.com>
2020-05-22 16:23:06 +0800

    WIP: use common configuration for all Mongoose connections

--- src/loaders/db.ts
+++ src/loaders/db.ts
@@ -4,5 +4,13 @@ import { createConnection } from 'mongoose';
 // LOADERS
 import { env } from './env';
 
-export const cacheDB = createConnection(env.MONGO_DB_CACHE_URL);
-export const sessionDB = createConnection(env.MONGO_DB_SESSION_URL);
+// GLOBAL VARIABLES
+const config: import('mongoose').ConnectionOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+};
+
+export const cacheDB = createConnection(env.MONGO_DB_CACHE_URL, config);
+export const sessionDB = createConnection(env.MONGO_DB_SESSION_URL, config);
