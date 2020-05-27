ccbeabf7d8dc9f03b456705334bf7b0f236ceebb
Basti Ortiz <ortizbasti16@gmail.com>
2020-05-27 20:29:35 +0800

    Feature: communicate to the user what went wrong during their request

--- src/errors/BaseError.ts
+++ src/errors/BaseError.ts
@@ -1,18 +1,28 @@
+// NODE CORE IMPORTS
+import { strict as assert } from 'assert';
+
+// DEPENDENCIES
+import { SafeString } from 'handlebars';
+
+// ERRORS
 import { API_ERROR_TYPES } from './ErrorTypes';
+import { SpotifyAPIError } from './SpotifyAPIError';
 
 export abstract class BaseError extends Error {
   /** Application-specific code for the error type. */
   readonly type: API_ERROR_TYPES;
   /** HTTP status code received in the failed fetch. */
   readonly status: number;
+  /** The human-readable description to be presented in the UI. */
+  readonly description: SafeString;
 
-  constructor(status: number, description: string, hint?: API_ERROR_TYPES) {
-    super(description);
+  constructor(status: number, message: string, hint?: API_ERROR_TYPES) {
+    super(message);
     this.status = status;
 
     if (status >= 500)
       this.type = API_ERROR_TYPES.EXTERNAL_ERROR;
-    else if (hint)
+    else if (typeof hint !== 'undefined')
       this.type = hint;
     else if (status === 429)
       this.type = API_ERROR_TYPES.RATE_LIMIT;
@@ -24,5 +34,39 @@ export abstract class BaseError extends Error {
       this.type = API_ERROR_TYPES.UNAUTHORIZED;
     else
       this.type = API_ERROR_TYPES.UNKNOWN;
+
+    switch (this.type) {
+      case API_ERROR_TYPES.EXTERNAL_ERROR:
+        this.description = new SafeString('The Spotify platform is temporarily unavailable. Sorry for the inconvenience! Please try again later.');
+        break;
+      case API_ERROR_TYPES.RATE_LIMIT:
+        assert(this instanceof SpotifyAPIError);
+        this.description = new SafeString(`Our servers have received so many requests that we have reached the maximum rate at which Spotify allows us to operate. Due to rate limiting, we cannot serve you right now. Please wait ${this.retryAfter} seconds before trying again. Sorry for the inconvenience!`);
+        break;
+      case API_ERROR_TYPES.INIT_FAILED:
+        this.description = new SafeString('We have encountered an error while initializing your session. We did not expect this behavior, so we have logged you out just to be safe. You may <a href="/login">log back in</a>, but if this issue persists, please contact the webmaster.');
+        break;
+      case API_ERROR_TYPES.REFRESH_FAILED:
+        this.description = new SafeString('We have encountered an error while refreshing your session. We did not expect this behavior, so we have logged you out just to be safe. You may <a href="/login">log back in</a>, but if this issue persists, please contact the webmaster.');
+        break;
+      case API_ERROR_TYPES.NO_PERMISSION:
+        this.description = new SafeString('We do not have the permission to access some information about you. You probably tampered with the redirect URL at Spotify. Although we must commend you for your sneaky attempt to fool our servers, we must invalidate your session just to be safe. You may <a href="/login">log back in</a>, but this time, please be careful. However, if you did not expect to see this issue, please contact the webmaster.');
+        break;
+      case API_ERROR_TYPES.UNAUTHORIZED:
+        this.description = new SafeString('We do not have the permission to access some information about you. You probably tampered with the redirect URL at Spotify. Although we must commend you for your sneaky attempt to fool our servers, we must invalidate your session just to be safe. You may <a href="/login">log back in</a>, but this time, please be careful. However, if you did not expect to see this issue, please contact the webmaster.');
+        break;
+      case API_ERROR_TYPES.FORBIDDEN:
+        this.description = new SafeString('Spotify has blocked our request to access some information about you. We did not expect this behavior, so we have logged you out just to be safe. You may <a href="/login">log back in</a>, but if this issue persists, please contact the webmaster.');
+        break;
+      case API_ERROR_TYPES.NOT_FOUND:
+        this.description = new SafeString('We cannot find some information about your favorite artists. We did not expect this behavior, so we have logged you out just to be safe. As an extra precaution, we have also removed everything we know about you and your profile from our databases. You may <a href="/login">log back in</a>, but if this issue persists, please contact the webmaster.');
+        break;
+      case API_ERROR_TYPES.ACCESS_DENIED:
+        this.description = new SafeString('No problem! If you ever change your mind, you\'re always welcome to <a href="/login">sign up</a> again.');
+        break;
+      default:
+        this.description = new SafeString('We have no idea how you received this error message. Please contact the webmaster immediately. You have found a bug in our system!');
+        break;
+    }
   }
 }
