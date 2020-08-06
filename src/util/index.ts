033661848eab1d74ae8b52209d651262c452f7e2
Basti Ortiz <ortizbasti16@gmail.com>
2020-08-06 19:59:39 +0800

    Feature: add utility function for generating randomized OAuth 2.0 PKCE-compliant code verifier strings

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,9 +1,11 @@
 import { formatEndpoint } from './formatEndpoint';
+import { generateRandomCodeVerifierChars } from './generateRandomCodeVerifierChars';
 import { getArrayDifference } from './getArrayDifference';
 import { subdivideArray } from './subdivideArray';
 
 export {
   formatEndpoint,
+  generateRandomCodeVerifierChars,
   getArrayDifference,
   subdivideArray,
 };
