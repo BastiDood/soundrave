656e22e554c9369764e07e3f56d58c5af79c7169
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2021-07-30 00:59:42 +0800

    Feat: cache artists and update user's profile of followed artists

--- api/src/worker.ts
+++ api/src/worker.ts
@@ -1,6 +1,7 @@
 /// <reference lib="deno.worker" />
 
 import { assert } from 'std/assert';
+import { Bson } from 'mongo';
 import { db } from 'db';
 
 import { FollowedArtists } from './model/spotify.ts';
@@ -13,17 +14,19 @@ async function processJob({ url, query, sessionId, userId, token }: Job) {
     const session = token ?? sessionCache.get(sessionId);
     assert(session, 'No session provided.');
 
+    // Fetch user's followed artists
+    const response = await fetch(url, {
+        headers: { Authorization: `Bearer ${session.accessToken}` },
+    });
+    const maybeResponse = await response.json();
+
     switch (query) {
         case JobType.GetFollowedArtists: {
-            // Fetch user's followed artists
-            const response = await fetch(url, {
-                headers: { Authorization: `Bearer ${session.accessToken}` },
-            });
-            const maybeFollowedArtists = await response.json();
-            const { next, items } = FollowedArtists.parse(maybeFollowedArtists);
+            const { next, items } = FollowedArtists.parse(maybeResponse);
 
             // FIXME: use bulk update for upserts
             // Cache the artists to the database
+            const ids: string[] = [];
             const artists = items.map(artist => {
                 setTimeout(processJob, 0, {
                     url: `https://api.spotify.com/v1/artists/${artist.id}/albums`,
@@ -31,6 +34,7 @@ async function processJob({ url, query, sessionId, userId, token }: Job) {
                     sessionId,
                     userId,
                 });
+                ids.push(artist.id);
                 return {
                     _id: artist.id,
                     name: artist.name,
@@ -38,8 +42,14 @@ async function processJob({ url, query, sessionId, userId, token }: Job) {
                 };
             });
 
-            // Cache the artists and update the user profile
-            await db.collection('artists').insertMany(artists, { ordered: false });
+            // Cache the artists and update the user's profile
+            const insertFuture = db.collection('artists').insertMany(artists, { ordered: false });
+            const updateFuture = db.collection('users').findAndModify(
+                {
+                    _id: Bson.ObjectId.createFromHexString(userId),
+                },
+                { upsert: false, update: { $addToSet: { followedArtists: { $each: ids } } } }
+            );
 
             // Create new followed artists job
             if (next)
@@ -50,6 +60,7 @@ async function processJob({ url, query, sessionId, userId, token }: Job) {
                     userId,
                 });
 
+            await Promise.all([insertFuture, updateFuture]);
             break;
         }
         case JobType.GetAlbums: {
@@ -58,6 +69,4 @@ async function processJob({ url, query, sessionId, userId, token }: Job) {
     }
 }
 
-self.onmessage = function (event: MessageEvent<Job>) {
-    return processJob(event.data);
-};
+self.onmessage = evt => processJob(evt.data as Job);
