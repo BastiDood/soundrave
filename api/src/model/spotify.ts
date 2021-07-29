cfec4fafc67223fc4c4609755934a921dc1bb70e
Basti Ortiz <39114273+Some-Dood@users.noreply.github.com>
2021-07-30 02:08:39 +0800

    Feat: add schemas for artists and albums

--- api/src/model/spotify.ts
+++ api/src/model/spotify.ts
@@ -7,23 +7,6 @@ function createCursorPagingObject<T>(schema: z.ZodSchema<T>) {
     });
 }
 
-const YearPrecise = z.object({
-    precision: z.literal('year'),
-    year: z.number().positive().int(),
-});
-
-const MonthPrecise = YearPrecise.extend({
-    precision: z.literal('month'),
-    month: z.number().positive().int(),
-});
-
-const DayPrecise = YearPrecise.extend({
-    precision: z.literal('day'),
-    month: z.number().positive().int(),
-});
-
-const SpotifyDate = z.union([YearPrecise, MonthPrecise, DayPrecise]);
-
 const Image = z.object({
     height: z.number().positive().int().nullable(),
     width: z.number().positive().int().nullable(),
